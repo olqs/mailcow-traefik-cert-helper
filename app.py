8529e4eb9262283b4aa8f9173765d9f8cc5b5b67
olqs <github@olqs.de>
2023-12-01 13:32:18 +0100

    updated config to new mysql module

--- app.py
+++ app.py
@@ -6,11 +6,11 @@ import os
 app = Flask(__name__)
 mysql = MySQL()
 
-app.config['MYSQL_DATABASE_USER'] = os.environ.get("MYSQL_DATABASE_USER", default='mailcow')
-app.config['MYSQL_DATABASE_PASSWORD'] = os.environ.get("MYSQL_DATABASE_PASSWORD", default='mailcow')
-app.config['MYSQL_DATABASE_PORT'] = os.environ.get("MYSQL_DATABASE_PORT", default=3306)
-app.config['MYSQL_DATABASE_DB'] = os.environ.get("MYSQL_DATABASE_DB", default='mailcow')
-app.config['MYSQL_DATABASE_HOST'] = os.environ.get("MYSQL_DATABASE_HOST", default='mysql-mailcow')
+app.config['MYSQL_USER'] = os.environ.get("MYSQL_DATABASE_USER", default='mailcow')
+app.config['MYSQL_PASSWORD'] = os.environ.get("MYSQL_DATABASE_PASSWORD", default='mailcow')
+app.config['MYSQL_PORT'] = os.environ.get("MYSQL_DATABASE_PORT", default=3306)
+app.config['MYSQL_DB'] = os.environ.get("MYSQL_DATABASE_DB", default='mailcow')
+app.config['MYSQL_HOST'] = os.environ.get("MYSQL_DATABASE_HOST", default='mysql-mailcow')
 
 TRAEFIK_ROUTER = os.environ.get("TRAEFIK_ROUTER", default="nginx-mailcow-secure")
 TRAEFIK_CERTRESOLVER = os.environ.get("TRAEFIK_CERTRESOLVER", default="http")
@@ -22,8 +22,7 @@ mysql.init_app(app)
 @app.route('/')
 def hello():
   try:
-    conn = mysql.connect()
-    cursor = conn.cursor()
+    cursor = mysql.connection.cursor()
     cursor.execute("SELECT domain from domain where active=1;")
     rows=cursor.fetchall()
     tls_domains = []
@@ -54,5 +53,4 @@ def hello():
     print(e)
   finally:
     cursor.close()
-    conn.close()
 
